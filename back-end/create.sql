8e7e9f44ecea80ecf0421b600086b271515acb2d
Clément Goujon <clment.goujon@etu.minesparis.psl.eu>
2026-01-16 10:14:45 +0100

    feat(backend::create.sql):add table User

--- back-end/create.sql
+++ back-end/create.sql
@@ -5,3 +5,20 @@ CREATE TABLE IF NOT EXISTS Roles(
   name string UNIQUE NOT NULL,
   description string DEFAULT "No description provided."
 )
+
+CREATE TABLE IF NOT EXISTS "User" (
+    "user_id" SERIAL PRIMARY KEY,
+    "username" UNIQUE TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "email" UNIQUE TEXT NOT NULL,
+    "phone_number" UNIQUE TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "password_salt" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "role_id" FOREIGN KEY REFERENCES "Role"("role_id") NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT TRUE,
+    "last_login" TIMESTAMP,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
