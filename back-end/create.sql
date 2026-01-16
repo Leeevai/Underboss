8294c12a6aa01a27557145ca5201d1b4c1da40f7
Leeevai <hassan.mohsen@etu.minesparis.psl.eu>
2026-01-16 10:28:20 +0100

    feat(backend::create.sql): add table User_Interest

--- back-end/create.sql
+++ back-end/create.sql
@@ -1,12 +1,12 @@
 
 
-CREATE TABLE IF NOT EXISTS "Role"(
+CREATE TABLE IF NOT EXISTS "ROLE"(
   "role_id" SERIAL8 PRIMARY KEY,
   "name" string UNIQUE NOT NULL,
   "description" string DEFAULT "No description provided."
 )
 
-CREATE TABLE IF NOT EXISTS "User" (
+CREATE TABLE IF NOT EXISTS "USER" (
     "user_id" SERIAL PRIMARY KEY,
     "username" UNIQUE TEXT NOT NULL,
     "password_hash" TEXT NOT NULL,
@@ -16,16 +16,23 @@ CREATE TABLE IF NOT EXISTS "User" (
     "password_salt" TEXT NOT NULL,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
-    "role_id" FOREIGN KEY REFERENCES "Role"("role_id") NOT NULL,
+    "role_id" FOREIGN KEY REFERENCES "ROLE"("role_id") NOT NULL,
     "is_active" BOOLEAN NOT NULL DEFAULT TRUE,
     "last_login" TIMESTAMP,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "Category"(
+CREATE TABLE IF NOT EXISTS "CATEGORY"(
     "category_id" SERIAL PRIMARY KEY,
     "name" UNIQUE TEXT NOT NULL,
     "description" TEXT DEFAULT "No description provided."
     "slug" TEXT UNIQUE NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS "USER_INTEREST"(
+    "user_interest_id" SERIAL PRIMARY KEY,
+    "user_id" FOREIGN KEY REFERENCES "USER"("user_id") NOT NULL,
+    "category_id" FOREIGN KEY REFERENCES "CATEGORY"("category_id") NOT NULL,
+    UNIQUE("user_id", "category_id")
 )
