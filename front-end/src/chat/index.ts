cc00ba8e6850f229d9976fa4eb22ba24e5bfb919
Leeevai <hassanmohseen22@gmail.com>
2026-02-06 01:15:05 +0100

    feat(frontend::chat::index): add chat module exports

---
+++ front-end/src/chat/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Chat module - components for messaging
+ * 
+ * Main Components:
+ * - ChatScreen: Main container with list/detail navigation
+ * - ChatList: List of all chat threads
+ * - ChatDetail: Individual chat conversation view
+ * - ChatBubble: Message bubble component
+ */
+
+export { default as ChatScreen } from './ChatScreen';
+export { default as ChatList } from './ChatList';
+export { default as ChatDetail } from './ChatDetail';
+export { default as ChatBubble } from './ChatBubble';
